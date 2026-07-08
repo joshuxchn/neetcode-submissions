964e3713d3b37eafc77df8f810f3f70acf766c6b
Joshua Chen <joshua86@illinois.edu>
2026-07-08 13:14:20 -0700

    Add: ternary-expression-parser - submission-1

---
+++ Data Structures & Algorithms/ternary-expression-parser/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def parseTernary(self, expression: str) -> str:
+        stack=[]
+        expression = expression[::-1]
+        i=0
+        while i != len(expression):
+            if expression[i] != '?':
+                stack.append(expression[i])
+            else:
+                true_condition = stack.pop()
+                stack.pop()
+                false_condition = stack.pop()
+                i+=1
+                if expression[i] == 'T':
+                    stack.append(true_condition)
+                else:
+                    stack.append(false_condition)
+            i += 1
+        return "".join(stack)
