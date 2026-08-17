f7f5fd0a26ef890d935bdd4e041b5ffb82d27f72
Joshua Chen <joshua86@illinois.edu>
2026-08-17 15:03:20 -0700

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        #case 1:
+            #left and right are equal to each other. recurse left, right
+        #case 2:
+            #center and right are equal. recurse center left, right right
+            #center and left are equal. recurse left left, center right
+        substring = s[0]
+        best_len = 1
+
+        def dfs(l, r):
+            nonlocal best_len
+            nonlocal substring
+            if l < 0 or r >= len(s):
+                return
+
+            if s[l] == s[r]:
+                if r-l+1 > best_len:
+                    substring = s[l:r+1]
+                    best_len = r-l+1
+                
+                #odd
+                dfs(l-1, r+1)
+
+
+        for i in range(len(s)-1):
+            dfs(i, i)
+            dfs(i, i + 1)
+        
+        return substring
