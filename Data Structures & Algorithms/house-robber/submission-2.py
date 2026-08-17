e7420547124a21a8a746b94168ada2272cd656b0
Joshua Chen <joshua86@illinois.edu>
2026-08-16 21:54:08 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [-1] * (len(nums))
+
+        cache[0] = nums[0]
+        if len(nums) == 1: return nums[0]
+        cache[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            #a: rob now, also robbed 2 prior
+            #b: rob now, didn't rob prior
+            cache[i] = max(nums[i] + cache[i-2], cache[i-1])
+
+        print(cache)
+        return cache[len(nums)-1]
+
