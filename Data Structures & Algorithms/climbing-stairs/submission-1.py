db72430c80e3df8f77dde5c3851859370fdc667a
Joshua Chen <joshua86@illinois.edu>
2026-08-16 18:41:21 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * n #stores how many unique ways at that level
+        def climb(i):
+            if i >= n:
+                return i == n
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = climb(i + 1) + climb(i + 2)
+            return cache[i]
+        
+        return climb(0)
