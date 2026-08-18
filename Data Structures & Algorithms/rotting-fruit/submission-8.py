e51a98d3242d4aec88237d14c34c5d9301e98a55
Joshua Chen <joshua86@illinois.edu>
2026-08-18 00:09:07 -0700

    Add: rotting-fruit - submission-8

---
+++ Data Structures & Algorithms/rotting-fruit/submission-8.py
@@ -0,0 +1,64 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        #count how many fresh banans there are, tally
+            #if fresh banaas is same after minute 0 to 1, return -1
+
+        #tally of minutes
+
+        
+        #start at the rotten bananas
+        #if fresh banana, mark as rotten
+        #each queue iteration is another minute, until queue is empty
+        #don't need visited set because marking as 2 is the same
+            #is the answer -1? because we only end when the queue is empty, which is after the last banana if filled, which will call again     
+
+        minutes = 0
+        fresh = 0
+        q = deque()
+        ROW, COL = len(grid), len(grid[0])
+
+        def rot(r, c):
+            nonlocal fresh
+            if min(r, c) < 0 or r==ROW or c==COL or grid[r][c] == 0 or grid[r][c] == 2:
+                return
+
+            grid[r][c] = 2 
+            fresh -= 1
+            #mark as rotten before to prevent adding same fresh fruit   multiple times
+            q.append((r,c))
+        s = 0
+        for r in range(ROW):
+            for c in range(COL):
+                x = grid[r][c]
+                s += x
+                if x == 1:
+                    fresh += 1
+                elif x == 2:
+                    q.append((r, c))
+
+        if s ==0: return 0 #nothing to process
+        if len(q) == 0: return -1 #no rotten oranges
+        
+        while q and fresh != 0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                
+                
+                #we need set because we keep adding rotten bananas
+                #but wont it stop once we hit fresh == 0?
+                #also if it's 2 we don't append...
+                rot(r + 1, c)
+                rot(r - 1, c)
+                rot(r, c + 1)
+                rot(r, c - 1)
+
+            minutes += 1
+
+        if fresh > 0:
+            return -1
+
+        return minutes
+
+
+
+
