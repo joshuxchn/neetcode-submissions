6412830e172fd51341bce7e0079636001d13166e
Joshua Chen <joshua86@illinois.edu>
2026-08-21 17:30:20 -0500

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #optional, efficient
+        if len(edges) != n-1:
+            return False #not enough edges to connect
+
+
+
+
+        #map to store neighbors of node
+        neighbors = {}
+        for i in range(n):
+            neighbors[i] = []
+        
+        for edge in edges:
+            neighbors[edge[0]].append(edge[1])
+            #undirected graph
+            neighbors[edge[1]].append(edge[0]) 
+        
+
+
+        #dfs cycle detection
+            #used set
+            #edge case: can't DFS back to the parent
+        visited = set()
+        def dfs(node, parent):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+
+            for neighbor in neighbors[node]:
+                if neighbor == parent:
+                    continue
+                if not dfs(neighbor, node):
+                    return False
+            
+            return True
+        x = dfs(0, None)
+        if len(visited) -1 != n-1: 
+            print(len(visited))
+            return False #unconnected
+        return x
+        
